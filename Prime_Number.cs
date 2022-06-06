f9aee16ec51a67aa33af04e24e0afd30bf2015d6
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-06-06 23:05:05 +0500

    Update Prime_Number.cs

--- Prime_Number.cs
+++ Prime_Number.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*if the Prime number is that which is only divisible by one and itself.we start the dividing from zero because every number is divible by one.
+if count increament then show it a not a prime number*/
+
 int a;
 
 Console.WriteLine("How many Numbers you want to Check the it's Prime or not");
@@ -8,6 +11,7 @@ for (int i = 0; i < a; i++)
     //Console.WriteLine("Enter a Number To Check Prime or ");
     int b=int.Parse(Console.ReadLine());
     int count = 0;
+    
     for (int div = 2; div*div <= b; div++)
     {
         if (b%div==0)
