85ab64eb2957eac8448e8798459b62276015a5eb
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-06-09 01:14:34 +0500

    Its Shows the Prime Numbers
    Its Shows the Prime Numbers between the Lower and upper range as you want.

---
+++ PrintAllPrimeNumbers.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!");
+int low,high;
+Console.WriteLine("Please enter the Start Number");
+low = int.Parse(Console.ReadLine());
+high = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the End Number");
+for (int a = low; a <=high; a++)
+{
+    int count=0;
+    for (int div = 2; div*div<=a; div++)
+    {
+        if (a%div==0)
+        {
+            count++;
+            break;
+        }
+    }
+    if (count == 0)
+    {
+        Console.WriteLine(a);
+    }
+}
