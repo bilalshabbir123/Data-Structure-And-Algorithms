626550778f444b3afe54c7d1f061e079e6df5535
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-09-29 12:33:47 +0500

    Add files via upload

---
+++ Digits_of_a_Number.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class Digits_of_a_Number
+    {
+       
+        static void Main(string[] args)
+        {
+            int digit = 0;
+            Console.WriteLine("Please Enter a Numebr");
+            int temp = int.Parse(Console.ReadLine());
+            int b = temp;
+            while (b != 0)
+            {
+                b = b / 10;
+                digit++;
+            }
+            int div = (int)Math.Pow(10, digit - 1);
+            while(div!=0)
+            {
+                int a = temp / div;
+                Console.WriteLine(a);
+                temp = temp % div;
+                div = div / 10;
+            }
+            //if we put while(temp!=0) then zero will not come out.we make the remaider zero
+        }
+    }
+}
