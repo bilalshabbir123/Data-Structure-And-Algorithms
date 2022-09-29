e66e8f0d0482acc17a035d7553146370a36c2e74
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-09-29 12:06:37 +0500

    Check the Digits in a Number.

---
+++ CountDigitsinNumebr.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class CountDigitsinNumebr
+    {
+       
+        static void Main(string[] args)
+        {
+            int digit = 0;
+            Console.WriteLine("Please Enter a Digit");
+            int a = int.Parse(Console.ReadLine());
+            while (a!=0)
+            {
+                a = a / 10;
+                digit++;
+            }
+            Console.WriteLine(digit);
+
+        }
+    }
+}
