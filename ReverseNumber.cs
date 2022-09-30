4f563fb6ef0695b67a545c11a5bb82b24e68c20f
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-09-30 11:17:22 +0500

    Add files via upload

---
+++ ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class ReverseNumber
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Numebr");
+            int n = int.Parse(Console.ReadLine());
+            while ( n> 0)
+            {
+               int digit = n % 10;
+                Console.Write(digit);
+                n = n / 10;
+
+            }
+
+        }
+    }
+}
