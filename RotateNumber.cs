23f0a4e71c8719295f0e98c44a3461624e558907
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-10-12 00:57:22 +0500

    Add files via upload

---
+++ RotateNumber.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class RotateNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter a rotation Number");
+            int k = int.Parse(Console.ReadLine());
+
+            int temp = n;
+            int nod = 0;
+            while(temp>0)
+            {
+                temp = temp / 10;
+                nod++;
+            }
+            k = k % nod;
+            if(k<0)
+            {
+                k = k + nod;
+            }
+            int div = 1;
+            int mul = 1;
+            for(int i = 1; i <=nod; i++)
+            {
+                if(i<=k)
+                {
+                    div = div * 10;
+                }
+                else
+                {
+                    mul = mul * 10;
+                }
+            }
+            int q = n / div;
+            int r = n % div;
+
+            int rot = r * mul + q;
+            Console.WriteLine(rot);
+        }
+    }
+}
