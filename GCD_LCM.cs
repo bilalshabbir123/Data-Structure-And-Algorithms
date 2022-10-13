6cba3f0b3dfb0c71baee7ff226603ab56fe7b508
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-10-13 12:11:57 +0500

    Add files via upload

---
+++ GCD_LCM.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class RotateNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a Second Number");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int on1 = n1;
+            int on2 = n2;
+            while(n1%n2!=0)
+            {
+                int rem = n1 % n2;
+                n1 = n2;
+                n2 = rem;
+            }
+            int gcd = n2;
+            int lcm = (on1 * on2) / gcd;
+            Console.WriteLine("GCD :"+gcd);
+            Console.WriteLine("LCM :"+lcm);
+           
+        }
+    }
+}
