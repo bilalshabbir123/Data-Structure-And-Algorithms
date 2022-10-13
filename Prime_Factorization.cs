34da70ddc0c8637381953510625d2a5906094155
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-10-13 12:41:43 +0500

    Add files via upload

---
+++ Prime_Factorization.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LearnDSAAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class Prime_Factorization
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number");
+            int n = int.Parse(Console.ReadLine());
+            for (int div = 2; div*div < n; div++)
+            {
+                while(n%div==0)
+                {
+                    n = n / div;
+                    Console.Write(div + " ");
+                }
+            }
+           if(n !=1)
+            {
+                Console.Write(n);
+            }
+        }
+    }
+}
